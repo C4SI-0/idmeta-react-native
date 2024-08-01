d53eb5d2ba224238389bcd6212fc8a5004a7ddfd
Timoor Ahmed <taimoorahm.ch@gmail.com>
2024-08-01 19:50:42 +0500

    update

--- android/src/main/java/com/idmetareactnative/IdmetaReactNativePackage.java
+++ android/src/main/java/com/idmetareactnative/IdmetaReactNativePackage.java
@@ -1,45 +1,25 @@
 package com.idmetareactnative;
 
-import androidx.annotation.Nullable;
-
-import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.module.model.ReactModuleInfo;
-import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.NativeModule;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class IdmetaReactNativePackage extends TurboReactPackage {
+public class IdmetaReactNativePackage implements ReactPackage {
 
-  @Nullable
-  @Override
-  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-    if (name.equals(IdmetaReactNativeModule.NAME)) {
-      return new IdmetaReactNativeModule(reactContext);
-    } else {
-      return null;
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new IdmetaReactNativeModule(reactContext));
+        return modules;
     }
-  }
 
-  @Override
-  public ReactModuleInfoProvider getReactModuleInfoProvider() {
-    return () -> {
-      final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-      boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-      moduleInfos.put(
-              IdmetaReactNativeModule.NAME,
-              new ReactModuleInfo(
-                      IdmetaReactNativeModule.NAME,
-                      IdmetaReactNativeModule.NAME,
-                      false, // canOverrideExistingModule
-                      false, // needsEagerInit
-                      true, // hasConstants
-                      false, // isCxxModule
-                      isTurboModule // isTurboModule
-      ));
-      return moduleInfos;
-    };
-  }
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
 }
